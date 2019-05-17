ed4fbcb1461760e14a48b9ede2e1c1554633d650
Jean-Loup Adde <jean-loup.adde@juanwolf.fr>
2019-05-17 23:01:01 +0100

    :sparkles:(): Lists all "deleted" users

--- go.mod
+++ go.mod
@@ -1,3 +1,9 @@
-module .
+module github.com/juanwolf/genkide
 
 go 1.12
+
+require (
+	github.com/gorilla/websocket v1.4.0 // indirect
+	github.com/nlopes/slack v0.5.1-0.20190515005541-e2954b1409b0
+	github.com/pkg/errors v0.8.1 // indirect
+)
--- main.go
+++ main.go
@@ -1,9 +1,32 @@
 package main
 
 import (
+	"flag"
 	"fmt"
+	"github.com/nlopes/slack"
 )
 
 func main() {
-	fmt.Println("Hello, World!")
+	slackToken := flag.String("t", "default", "A personal Slack Token")
+	flag.Parse()
+
+	api := slack.New(*slackToken)
+	users, err := api.GetUsers()
+
+	if err != nil {
+		fmt.Printf("%s\n", err)
+		return
+	}
+
+	if len(users) == 0 {
+		fmt.Println("No users found!")
+	}
+
+	fmt.Println("Deleted users:")
+
+	for _, user := range users {
+		if user.Deleted {
+			fmt.Println("-", fmt.Sprintf("%s(%s)", user.Name, user.RealName), "updated", user.Updated)
+		}
+	}
 }
